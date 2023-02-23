5377cb0fdb1ed4a6901154c13c20442d3d16cc72
Run Huang <19307110301@fudan.edu.cn>
2023-02-23 14:47:38 +0800

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,7 @@ const FooterBox = styled((props) => (
 export default function Footer(props) {
     return (
         <FooterBox>
-            © Run Huang. Last Updated: Dec. 21, 2022
+            © Run Huang. Last Updated: Feb. 23, 2023
         </FooterBox>
     );
 };
