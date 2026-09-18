d5c67c44f4bafde7b6c6fd89dbee261205c56c0a
Run Huang <77979094+itsrun@users.noreply.github.com>
2026-09-18 14:12:27 -0700

    update

--- app/section/pub.tsx
+++ app/section/pub.tsx
@@ -54,7 +54,7 @@ function VideoPlayer({
       <img
         src={cover}
         alt="Video thumbnail"
-        className="w-full h-full object-contain grayscale opacity-75 group-hover/publication:grayscale-0 group-hover/publication:opacity-100 transition-all duration-300"
+        className="w-full h-full object-contain grayscale opacity-75 group-hover/play:grayscale-0 group-hover/play:opacity-100 transition-all duration-300"
       />
       <div className="absolute inset-0 bg-black/10 group-hover/play:bg-black/20 transition-colors duration-200" />
       <div className="absolute inset-0 flex items-center justify-center">
@@ -79,14 +79,17 @@ function PubItem(item: PubItemProps) {
   const { title, authors, link, venue, award, id, video, paper, cover, note } =
     item;
   return (
-    <div className="my-auto px-1 group/publication relative" id={id}>
+    <div
+      className="my-auto px-1 hover:bg-[#f3f3f3] transition-colors duration-200 relative"
+      id={id}
+    >
       <a
         href={link || paper || video}
         target="_blank"
         rel="noopener noreferrer"
         className="absolute inset-0 opacity-0"
       />
-      <h3 className="font-medium leading-tight sm:leading-snug text-lg group-hover/publication:underline underline-offset-2">
+      <h3 className="font-medium leading-tight sm:leading-snug text-lg">
         {title}
       </h3>
       {award && <p className="text-amber-700 mt-0.25">{award}</p>}
